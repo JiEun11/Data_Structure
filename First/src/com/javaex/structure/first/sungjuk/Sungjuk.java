993d20babf9fe45e8a8882898a71b5393d18e88e
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-09 22:03:57 +0900

    성적처리 프로그램 수정

--- First/src/com/javaex/structure/first/sungjuk/Sungjuk.java
+++ First/src/com/javaex/structure/first/sungjuk/Sungjuk.java
@@ -1,68 +1,81 @@
 package com.javaex.structure.first.sungjuk;
 
+class Student {
+
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int total;
+	private float avg;
+
+	public Student() {
+	}
+
+	public Student(String name, int kor, int eng, int math) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.total = kor + eng + math;
+		this.avg = total/3.f;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public int getTotal() {
+		return total = kor+eng+math;
+	}
+
+	public float getAvg() {
+		return avg = getTotal()/3.f;
+	}
+	
+	public String toString() {
+		return name+"\t"+kor+"\t"+eng+"\t"+math+"\t"+total+"\t"+avg;
+	}
+	
+	public void showInfo() {
+		System.out.println("학생: " + this.name);
+		System.out.println("국어점수: " + this.kor);
+		System.out.println("영어점수: " + this.eng);
+		System.out.println("수학점수: " + this.math);
+	}	
+}
+
 public class Sungjuk {
-
+	
+	private Student []stu;
+	
+	public Sungjuk(int num) {
+		stu = new Student[num];
+	}
+	
+	public void input() {
+		for(int i = 0; i<stu.length; i++) {
+			stu[i] = new Student();
+			stu[i].setName(sc.next());
+			
+			Student str = new Student(name, kor, eng, math);
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	
 	}
-
-	public class Student {
-
-		private String name;
-		private int kor;
-		private int eng;
-		private int math;
-
-		public Student() {
-		}
-
-		public Student(String name, int kor, int eng, int math) {
-			this.setName(name);
-			this.setKor(kor);
-			this.setEng(eng);
-			this.setMath(math);
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public int getKor() {
-			return kor;
-		}
-
-		public void setKor(int kor) {
-			this.kor = kor;
-		}
-
-		public int getEng() {
-			return eng;
-		}
-
-		public void setEng(int eng) {
-			this.eng = eng;
-		}
-
-		public int getMath() {
-			return math;
-		}
-
-		public void setMath(int math) {
-			this.math = math;
-		}
-
-		public void showInfo() {
-			System.out.println("학생: " + this.name);
-			System.out.println("국어점수: " + this.kor);
-			System.out.println("영어점수: " + this.eng);
-			System.out.println("수학점수: " + this.math);
-		}
-
-	}
-
 }
