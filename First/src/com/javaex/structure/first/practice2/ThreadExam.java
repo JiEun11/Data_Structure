1266a0d734aa56309c3a1ea6500385f9160b9587
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-10 12:39:15 +0900

    21.12.10. thread extends해서 사용해보기

---
+++ First/src/com/javaex/structure/first/practice2/ThreadExam.java
@@ -0,0 +1,36 @@
+package com.javaex.structure.first.practice2;
+
+public class ThreadExam extends Thread{
+
+	private int []arr;
+	
+	public ThreadExam() {
+		arr = new int[4]; //생성자로 arr
+		
+		for(int i=0; i<arr.length;i++) {
+			arr[i]=i;
+		}
+	}
+	
+	public void run() {
+		for(int i=0; i<arr.length; i++) {
+			try {
+				sleep(100);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+			
+			System.out.println(arr[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThreadExam te = new ThreadExam();   //thread 생성
+		te.start();   // thread runnable 상태로 (대기상태) 스케줄러가 보고있다가 너 동작해 라고 함.
+		
+				
+	}
+
+}
