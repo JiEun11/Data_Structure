2f70ee050a9c0b4856e6ee0aebf4ce785c5c1e09
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-10 21:27:46 +0900

    21.12.10 성적처리프로그램 HashSet v1

--- First/src/com/javaex/structure/second/test/SungjukSet.java
+++ First/src/com/javaex/structure/second/test/SungjukSet.java
@@ -1,144 +1,18 @@
 package com.javaex.structure.second.test;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
-//Student 정보를 담는 클래스
+// Student Class는 ArrayList 클래스에 있음.
 
-class Student {
-
-	private String name;
-
-	private int kor;
-
-	private int eng;
-
-	private int math;
-
-	private int total;
-
-	private float avg;
-
-	public Student() {
-
-	}
-
-	public Student(String name, int kor, int eng, int math) {
-
-		this.name = name;
-
-		this.kor = kor;
-
-		this.eng = eng;
-
-		this.math = math;
-
-		this.total = kor + eng + math;
-
-		this.avg = total / 3.f;
-
-	}
-
-	public String getName() {
-
-		return name;
-
-	}
-
-	public void setName(String name) {
-
-		this.name = name;
-
-	}
-
-	public int getKor() {
-
-		return kor;
-
-	}
-
-	public void setKor(int kor) {
-
-		this.kor = kor;
-
-	}
-
-	public int getEng() {
-
-		return eng;
-
-	}
-
-	public void setEng(int eng) {
-
-		this.eng = eng;
-
-	}
-
-	public int getMath() {
-
-		return math;
-
-	}
-
-	public void setMath(int math) {
-
-		this.math = math;
-	}
-
-	public int getTotal() {
-
-		return total = kor + eng + math;
-
-	}
-	
-	public void setTotal(int total) {
-		this.total = total;
-	}
-	
-	public float getAvg() {
-
-		return avg = getTotal() / 3.f;
-	}
-	
-	public void setAvg(float avg) {
-		this.avg = avg;
-	}
-
-	public String toString() {
-
-		return "이름: " + name + "\t" + "국어점수: " + kor + "\t" + "영어점수: " + eng + "\t" + "수학점수: " + math + "\t" + "합계: "
-				+ getTotal() + "\t" + "평균: " + getAvg();
-
-	}
-
-	// Set에서 객체 중복 처리
-	@Override
-	public int hashCode() {
-		
-		return (this.name + this.kor + this.eng + this.math).hashCode();
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if(obj instanceof Student) {
-			Student other = (Student)obj;
-			return (other.name==this.name && other.kor==this.kor 
-					&& other.eng == this.eng && other.math== this.math);
-		}
-		return super.equals(obj);
-	}
-}
-
-public class SungjukArrayList {
+public class SungjukSet {
 
 	static int num; // 학생 명수 받을 사용자 값
-	private ArrayList<Student> stu; // Student 객체 타입의 ArrayList인 stu 선언
+	private HashSet<Student> stu; // Student 객체 타입의 HashSet인 stu 선언
 
-	public SungjukArrayList(int numStudent) { // 사용자 생성자로 ArrayList 용량 정의
+	public SungjukSet(int numStudent) { // 사용자 생성자로 HashSet 용량 정의
 
-		stu = new ArrayList<Student>(numStudent); // Student 객체 타입의 ArrayList Heap 메모리에 올림.
-		
+		stu = new HashSet<Student>(numStudent); // Student 객체 타입의 HashSet을 Heap 메모리에 올림.
 	}
 
 	public void input() {
@@ -173,9 +47,7 @@ public class SungjukArrayList {
 //			stu.add(new Student(name, kor, eng, math));
 //			Student student = new Student(name, kor, eng, math);
 //			해주고 stu.add(student); 해주던지
-
 		}
-
 	}
 
 //학생 이름 입력 시 성적 조회
@@ -263,7 +135,7 @@ public class SungjukArrayList {
 		
 		num = sc.nextInt();
 		
-		SungjukArrayList sj = new SungjukArrayList(num);
+		SungjukSet sj = new SungjukSet(num);
 
 		sj.input();
 
@@ -306,5 +178,3 @@ public class SungjukArrayList {
 	}
 
 }
-
-//Student 정보를 담는 클래스
