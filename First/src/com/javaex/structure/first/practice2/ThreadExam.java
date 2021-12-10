06e16b489f262aa8358969adb18c277d970342a4
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-10 14:01:46 +0900

    Thread 실습 - implements runnable

--- First/src/com/javaex/structure/first/practice2/ThreadExam.java
+++ First/src/com/javaex/structure/first/practice2/ThreadExam.java
@@ -1,6 +1,6 @@
 package com.javaex.structure.first.practice2;
 
-public class ThreadExam extends Thread{
+public class ThreadExam implements Runnable{
 
 	private int []arr;
 	
@@ -12,24 +12,26 @@ public class ThreadExam extends Thread{
 		}
 	}
 	
+	@Override
 	public void run() {
 		for(int i=0; i<arr.length; i++) {
 			try {
-				sleep(100);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				
 				e.printStackTrace();
 			}
 			
-			System.out.println(currentThread() + " " + arr[i]);
+			System.out.println(Thread.currentThread() + " " + arr[i]);
 		}
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ThreadExam te = new ThreadExam();   //thread 생성
-		te.start();   // thread runnable 상태로 (대기상태) 스케줄러가 보고있다가 너 동작해 라고 함.
-		
+//		te.start();   // thread runnable 상태로 (대기상태) 스케줄러가 보고있다가 너 동작해 라고 함.
+		Thread th = new Thread(te);  //내가 만든 러너블 te를 넣어줌 ---> 동적 바인딩 
+		th.start();
 				
 	}
 
