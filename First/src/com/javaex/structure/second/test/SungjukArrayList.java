197cd6458663397d5bffc7e14eead4405fc38511
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-10 19:01:16 +0900

    21.12.10. 성적처리 프로그램 ArrayList v2

--- First/src/com/javaex/structure/second/test/SungjukArrayList.java
+++ First/src/com/javaex/structure/second/test/SungjukArrayList.java
@@ -3,7 +3,6 @@ package com.javaex.structure.second.test;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 //Student 정보를 담는 클래스
 
 class Student {
@@ -100,52 +99,57 @@ class Student {
 
 	}
 
-	public String toString() {
-
-		return "이름: " + name + "\t" + "국어점수: " + kor + "\t" + "영어점수: " + eng + "\t" + "수학점수: " + math + "\t" + "합계: "
-				+ total + "\t" + "평균: " + avg;
-
-	}
+//	public String toString() {
+//
+//		return "이름: " + name + "\t" + "국어점수: " + kor + "\t" + "영어점수: " + eng + "\t" + "수학점수: " + math + "\t" + "합계: "
+//				+ total + "\t" + "평균: " + avg;
+//
+//	}
 }
 
 public class SungjukArrayList {
 
+	static int num; // 학생 명수 받을 사용자 값
 	private ArrayList<Student> stu; // Student 객체 타입의 ArrayList인 stu 선언
 
-	public SungjukArrayList(int num) {	// 사용자 생성자로 ArrayList 용량 정의
+	public SungjukArrayList(int numStudent) { // 사용자 생성자로 ArrayList 용량 정의
 
-		stu = new ArrayList<Student>(num);  //Student 객체 타입의 ArrayList Heap 메모리에 올림.
-		
+		stu = new ArrayList<Student>(numStudent); // Student 객체 타입의 ArrayList Heap 메모리에 올림.
+		System.out.println(stu.size());
 	}
-	
+
 	public void input() {
+		Scanner sc = new Scanner(System.in); // for문안에 X
 
-		for (int i = 0; i < stu.size(); i++) {
+		for (int i = 0; i < num; i++) {
 
-			Scanner sc = new Scanner(System.in);
-
-//			Student student = new Student();
+			Student student = new Student();
 
 			System.out.print("이름을 입력하세요: ");
-			String name = sc.next();
-//			student.setName(sc.next());
-			
+			student.setName(sc.next());
+
 			System.out.print("국어 점수를 입력하세요: ");
-			int kor = sc.nextInt();
-			
-//			student.setKor(sc.nextInt()); // 국어 점수 입력
+
+			student.setKor(sc.nextInt()); // 국어 점수 입력
 
 			System.out.print("영어 점수를 입력하세요: ");
-			int eng = sc.nextInt();
-//			student.setEng(sc.nextInt()); // 영어 점수 입력
+
+			student.setEng(sc.nextInt()); // 영어 점수 입력
 
 			System.out.print("수학 점수를 입력하세요: ");
-			int math = sc.nextInt();
-//			student.setMath(sc.nextInt()); // 수학 점수 입력
-			
-			stu.add(new Student(name, kor, eng, math));
-			stu.add(Student(student.getName(), student.getKor(), student.getEng(), student.getMath()));
-//Student stu = new Student(name, kor, eng, math); // 다 넣어주고 객체 생성
+
+			student.setMath(sc.nextInt()); // 수학 점수 입력
+
+			stu.add(student); // ArrayList는 객체를 갖고 다루는 애라 객체 넣어줘야함.
+
+//			만약 생성자로 add에 넣어줄꺼면 값 다 입력 받고 생성자를 넣던지
+//			String name = sc.next();
+//			int kor = sc.nextInt();
+//			int eng = sc.nextInt();
+//			int math = sc.nextInt();
+//			stu.add(new Student(name, kor, eng, math));
+//			Student student = new Student(name, kor, eng, math);
+//			해주고 stu.add(student); 해주던지
 
 		}
 
@@ -161,19 +165,19 @@ public class SungjukArrayList {
 
 		String nameSearch = sc.next();
 
-		for (int i = 0; i < stu.length; i++) {
+		for (Student student : stu) {
 
-			if (stu[i].getName().equals(nameSearch)) {
+			if (student.getName().contains(nameSearch)) {
 
-				System.out.println("국어 점수: " + stu[i].getKor());
+				System.out.println("국어 점수: " + student.getKor());
 
-				System.out.println("영어 점수: " + stu[i].getEng());
+				System.out.println("영어 점수: " + student.getEng());
 
-				System.out.println("수학 점수: " + stu[i].getMath());
+				System.out.println("수학 점수: " + student.getMath());
 
-				System.out.println("합 계: " + stu[i].getTotal());
+				System.out.println("합 계: " + student.getTotal());
 
-				System.out.println("평 균: " + stu[i].getAvg());
+				System.out.println("평 균: " + student.getAvg());
 
 			}
 
@@ -185,11 +189,11 @@ public class SungjukArrayList {
 
 	public void searchAvg50() {
 
-		for (int i = 0; i < stu.length; i++) {
+		for (Student student : stu) {
 
-			if (stu[i].getAvg() >= 50) {
+			if (student.getAvg() >= 50) {
 
-				System.out.println(stu[i].getName());
+				System.out.println(student.getName());
 
 			}
 
@@ -201,12 +205,27 @@ public class SungjukArrayList {
 
 	public void showInfo() {
 
-		for (int i = 0; i < stu.length; i++) {
-
-			System.out.println(stu[i].toString());
-
+//		for (int i = 0; i < stu.size(); i++) {
+//
+//			System.out.println(stu[i].toString());
+//
+//		}
+		for (Student student : stu) {
+			System.out.println(student);
 		}
 
+	}
+
+// 원하는 학생 삭제
+	public void deleteInfo() {
+		System.out.print("삭제할 학생의 이름을 입력하세요.: ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		for (Student student : stu) {
+			if (student.getName().contains(name)) {
+				stu.remove(student);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
@@ -226,9 +245,9 @@ public class SungjukArrayList {
 
 		while (stop) {
 
-			System.out.println("1.학생 성적 조회 | 2.평균 50점이상 학생 조회 | 3.전체 목록 조회 | 4.프로그램 종료");
+			System.out.println("1.학생 성적 조회 | 2.평균 50점이상 학생 조회 | 3.전체 목록 조회 | 4.정보 삭제 | 5.프로그램 종료");
 
-			int num = sc.nextInt();
+			num = sc.nextInt();
 
 			if (num == 1) {
 
@@ -244,14 +263,16 @@ public class SungjukArrayList {
 
 			} else if (num == 4) {
 
+				sj.deleteInfo();
+
+			} else if (num == 5) {
+
 				System.out.println("프로그램이 종료됩니다.");
 
 				stop = false;
 
 			} else {
-
 				System.out.println("숫자 1부터 5까지 중에 입력해주세요. ");
-
 			}
 
 		}
