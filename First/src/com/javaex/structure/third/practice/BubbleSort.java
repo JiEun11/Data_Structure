0dc3bbddb3d155e7edb56d516cb60c7fcbbd91ab
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-13 10:26:47 +0900

    21.12.13. Bubble Sort v2 (random 함수 사용)

--- First/src/com/javaex/structure/third/practice/BubbleSort.java
+++ First/src/com/javaex/structure/third/practice/BubbleSort.java
@@ -7,11 +7,12 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		createBubbleSort();
 
-	}
-	
+	}	
 	private static void createBubbleSort() {
-		int data[] = new int[]{90, 78, 100, 30, 55};
-		
+		int data[] = new int[100];
+		for(int i = 0 ; i < 100; i++) {
+			data[i] = (int)((Math.random()*100)+0);
+		}
 		for(int i = 1; i < data.length; i++) {
 			for(int j = 0; j < data.length-i ; j++) {
 				if(data[j] > data[j+1]) {
