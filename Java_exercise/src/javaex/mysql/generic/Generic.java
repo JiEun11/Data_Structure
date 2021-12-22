dc962682e0bd6e4e01fc43acde0caf8814be0dec
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-22 19:55:17 +0900

    21.12.22 java generic 실습-2

--- Java_exercise/src/javaex/mysql/generic/Generic.java
+++ Java_exercise/src/javaex/mysql/generic/Generic.java
@@ -1,16 +1,44 @@
 package javaex.mysql.generic;
 
-class Person<T>{
-	public T info;
+class StudentInfo{
+	public int grade;
+	StudentInfo(int grade){
+		this.grade = grade;
+	}
+}
+
+class StudentPerson{
+	public StudentInfo info;
+	StudentPerson(StudentInfo info){
+		this.info = info;
+	}
+}
+
+class EmployeeInfo{
+	public int rank;
+	EmployeeInfo(int rank){
+		this.rank = rank;
+	}
+}
+
+class EmployeePerson{
+	public EmployeeInfo info;
+	EmployeePerson(EmployeeInfo info){
+		this.info = info;
+	}
 }
 
 public class Generic {
 
 	public static void main(String[] args) {
 		
-		Person<String> p1 = new Person<String>();
-		Person<StringBuilder> p2 = new Person<StringBuilder>();
-
+		StudentInfo si = new StudentInfo(2);
+		StudentPerson sp = new StudentPerson(si);
+		System.out.println(sp.info.grade);
+		
+		EmployeeInfo ei = new EmployeeInfo(1);
+		EmployeePerson ep = new EmployeePerson(ei);
+		System.out.println(ep.info.rank);
 	}
 
 }
