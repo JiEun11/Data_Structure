02860fd350c03750c4f1370f2b5bb7fe6a18fbca
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-22 21:19:30 +0900

    21.12.22 java generic 실습 v4- 복수의 generic abstract

--- Java_exercise/src/javaex/mysql/generic/Generic.java
+++ Java_exercise/src/javaex/mysql/generic/Generic.java
@@ -3,35 +3,33 @@ package javaex.mysql.generic;
 import java.util.Arrays;
 import java.util.List;
 
-class EmployeeInfo{
+abstract class Info{
+	public abstract int getLevel();
+}
+
+class EmployeeInfo extends Info{
 	public int rank;
 	EmployeeInfo(int rank){
 		this.rank = rank;
 	}
+	public int getLevel() {
+		return this.rank;
+	}
 }
 
-class Person<T, S>{
+class Person<T extends Info>{
 	public T info;
-	public S id;
-	Person(T info, S id){
+	Person(T info){
 		this.info = info;
-		this.id = id;
-	}
-	public <U> void printInfo(U info) {
-		System.out.println(info);
+		info.getLevel();
 	}
 }
-
 
 public class Generic {
 
 	public static void main(String[] args) {
-		EmployeeInfo e = new EmployeeInfo(1);
-		Integer i = new Integer(10);
-		Person p1 = new Person(e,i);
-		
-		p1.<EmployeeInfo>printInfo(e);
-		
+		Person<EmployeeInfo> p1 = new Person<EmployeeInfo>(new EmployeeInfo(1));
+		Person<String> p2 = new Person<String>("부장");
 //		p1.printInfo(e); 로 생략 가능 
 		
 
