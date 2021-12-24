7464eb2c6cbf189570d2ec27dfb6ba8f6cba6d8a
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-24 11:38:18 +0900

    21.12.24. Programmers - 정렬 v1

---
+++ Java_exercise/src/javaex/codingtest/SortArray.java
@@ -0,0 +1,20 @@
+package javaex.codingtest;
+
+public class SortArray {
+
+	class Solution {
+	    public int[] solution(int[] array, int[][] commands) {
+	        int[] answer = new int[commands.length];
+	        
+	        
+	        
+	        return answer;
+	    }
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
