4e1f88b91da6e7e25e200f4b35856720807328bf
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-28 15:35:25 +0900

    21.12.28 web 실습 2

---
+++ edu/src/main/java/base/VisitorGetServlet.java
@@ -0,0 +1,34 @@
+package base;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/visitorget")
+public class VisitorGetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("stname");
+		String op = request.getParameter("op");
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		LocalDate date = LocalDate.now();
+		String now = date.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일"));
+
+		out.print("<h2>" + name + " 님이 " + now + "에 남긴 글입니다.</h2>");
+		out.print("<hr>");
+		out.print("내용: " + op);
+		out.print("<p>");
+		out.print("<a href='/edu/htmlexam/visitorForm.html'>" + "입력화면으로... </a>");
+	}
+
+}
