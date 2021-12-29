81f59be19eb429fdd83f1315abcd1a7e5c3c46fe
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-29 09:06:09 +0900

    21.12.29 k번째수 수정 v2

--- Java_exercise/src/javaex/codingtest/Solution.java
+++ Java_exercise/src/javaex/codingtest/Solution.java
@@ -1,5 +1,7 @@
 package javaex.codingtest;
 
+import java.util.Arrays;
+
 public class Solution {
 
 	public int[] solution(int[] array, int[][] commands) {
@@ -7,7 +9,33 @@ public class Solution {
 		// commands 0,1,2 요소들의 각 요소들 또 뽑기
 		// 뽑은 요소들을 이용해서 array 자르기
 		// 낼 수정하기
-		int[] answer = {};
-		return answer;
-	}
+		int[] temp = new int[3];
+		int[] answer = new int[commands.length];
+		
+		int start, end, pick, i = 0;
+
+		for(int[] values : commands) {
+			temp= values;
+			start = temp[0];
+			end = temp[1];
+			pick = temp[2];
+			int[] copyArr = new int[end-start+1];
+//			copyArr = Arrays.copyOfRange(array, start-1, end);
+			System.arraycopy(array, start-1, copyArr, 0, end-start+1);
+			Arrays.sort(copyArr);
+			System.out.println(Arrays.toString(copyArr));
+			System.out.println(copyArr[pick-1]);
+			if(i<answer.length) {
+				answer[i++] = copyArr[pick-1];
+				System.out.println(answer[i]);
+			}
+			else {
+				break;
+			}
+		}
+
+//		System.out.println(Arrays.toString(temp));
+	return answer;
+}
+
 }
--- Java_exercise/src/javaex/codingtest/SortArray.java
+++ Java_exercise/src/javaex/codingtest/SortArray.java
@@ -1,27 +1,16 @@
 package javaex.codingtest;
 
+import java.util.Arrays;
+
 public class SortArray {
-	// 낼 수정하기 
-	public int[] solution(int[] array, int[][] commands) {
-
-		int[] temp = {};
-
-		for (int[] idx : commands) {
-			int i = 0;
-			System.out.println(idx[i++]);
-		}
-
-		int[] answer = {};
-
-		return answer;
-	}
-
 
 	public static void main(String[] args) {
 		int[] array = { 1, 5, 2, 6, 3, 7, 4 };
 		int[][] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
 		Solution sol = new Solution();
-		sol.solution(array, commands);
+		int[] answ = sol.solution(array, commands);
+		
+		System.out.println(Arrays.toString(answ));
 	}
 
 }
