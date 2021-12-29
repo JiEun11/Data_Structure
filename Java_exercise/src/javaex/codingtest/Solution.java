276d52c0901ca42abcbaf6fb14db8823f59d8efa
BIT <BIT@DESKTOP-MIG2BMV>
2021-12-30 08:31:20 +0900

    21.12.30 programmers k번째 수 완성 v2

--- Java_exercise/src/javaex/codingtest/Solution.java
+++ Java_exercise/src/javaex/codingtest/Solution.java
@@ -19,21 +19,26 @@ public class Solution {
 			start = temp[0];  // commands의 0번째 요소 (시작 인덱스)
 			end = temp[1];    // commands의 1번째 요소 (끝 인덱스)
 			pick = temp[2];   // commands의 2번째 요소 (뽑을 인덱스)
+			
 			int[] copyArr = new int[end-start+1];  // original array인 array를 복사할 배열 
-//			copyArr = Arrays.copyOfRange(array, start-1, end);
 			System.arraycopy(array, start-1, copyArr, 0, end-start+1);   // array배열 복사 후 copyArr에 저장 
+//			int[] copyArr = Arrays.copyOfRange(array, start-1, end);  //아니면 이렇게!!
+			
 			Arrays.sort(copyArr);  // copyArr 정렬
 //			System.out.println(Arrays.toString(copyArr));  // 맞게 되었나 체크
-			System.out.println(copyArr[pick-1]);	//  뽑은 인덱스 맞나 확인
-			if(i<answer.length) {
-				answer[i++] = copyArr[pick-1];    // answer 배열에 고른 인덱스 값 넣기 
-			}
-			else {
-				break;
-			}
+//			System.out.println(copyArr[pick-1]);	//  뽑은 인덱스 맞나 확인
+			
+//			i가 애초에 answer 범위를 넘을 일이 없구나...
+//			if(i<answer.length) {
+//				answer[i++] = copyArr[pick-1];    // answer 배열에 고른 인덱스 값 넣기 
+//			}
+//			else {
+//				break;
+//			}
+			answer[i++] = copyArr[pick-1]; //이 코드로 끝남! if문 필요 없음
+			
 		}
 
-//		System.out.println(Arrays.toString(temp));
 	return answer;
 }
 
