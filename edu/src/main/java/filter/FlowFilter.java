5d24c70fe1a37c336416638cce894adbbde04644
BIT <BIT@DESKTOP-MIG2BMV>
2022-01-06 10:40:49 +0900

    22.01.06 meeting 실습 & filter 실습

---
+++ edu/src/main/java/filter/FlowFilter.java
@@ -0,0 +1,52 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class FlowFilter
+ */
+@WebFilter(dispatcherTypes = {DispatcherType.REQUEST }
+					, urlPatterns = { "/first" })
+public class FlowFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FlowFilter() {
+        System.out.println("FlowFilter 객체 생성");
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
